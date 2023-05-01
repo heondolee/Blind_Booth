1d7eebebb3fc33f61664f0c6491396094bccfb27
heondo <rundo999@gmail.com>
2023-05-01 14:27:53 +0900

    카카오 로그인 완성 및 정리

--- blind/urls.py
+++ blind/urls.py
@@ -11,8 +11,6 @@ app_name = "blind"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.reservation, name='reservation'),
-    path("login/", views.login_view.as_view(), name="log_in"),
     path("logout/", views.log_out, name="log_out"),
-    # path("signup/", views.sign_up, name="sign_up"),
     path("accounts/", include('allauth.urls'))
 ]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) +static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- blind/views.py
+++ blind/views.py
@@ -9,44 +9,7 @@ from . import forms
 def reservation(request):
     return render(request, "reservation.html")
 
-class login_view(View):
-    def get(self, request):
-        form = forms.LoginForm()
-        context = {
-            "form": form,
-        }
-        return render(request, "reservation.html", context)
-    
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
-            user=authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return render(request, "reservation.html")
-        context = {
-            "forms":form,
-        }
-        return render(request, "reservation.html", context=context)
-
 @login_required(login_url='log_in')
 def log_out(request):
     logout(request)
     return redirect("blind:reservation")
-
-# def sign_up(request):
-#     if request.method == "POST":
-#         form = forms.SignupForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             return render(request, "reservation.html")
-#         return redirect("blind:reservation")
-#     else:
-#         form = forms.SignupForm()
-#         context = {
-#             "form":form,
-#         }
-#         return render(request, "reservation.html", context=context)
