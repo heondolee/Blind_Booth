dd0f4a0e22adb6a96d0f2552d929163238f7d7f3
heondo <rundo999@gmail.com>
2023-05-05 00:14:49 +0900

    문자 보내기 성공

--- blind/views.py
+++ blind/views.py
@@ -11,15 +11,60 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
 from django.contrib import messages
+import requests
+
+import time
+import hashlib
+import hmac
+import base64
+
+
+def	make_signature():
+    timestamp = str(int(time.time() * 1000))
+    access_key = "m2ly34gJo3yRuuy2TS5y"
+    secret_key = bytes('8H6SWmNYKXthJ6tWfO5WBNtJAt5wQrGhCvl0yRnz', 'UTF-8')
+    method = "POST"
+    message = method + " " + '/sms/v2/services/ncp:sms:kr:307397418568:blind_date/messages' + "\n" + timestamp + "\n" + access_key
+    message = bytes(message, 'UTF-8')
+    signingKey = base64.b64encode(hmac.new(secret_key, message, digestmod=hashlib.sha256).digest())
+    return signingKey
+
+
+
+def send_sms():
+    timestamp = str(int(time.time() * 1000))
+
+    url = 'https://sens.apigw.ntruss.com/sms/v2/services/ncp:sms:kr:307397418568:blind_date/messages'
+    headers = {
+        'Content-Type': 'application/json; charset=utf-8',
+        'x-ncp-apigw-timestamp': timestamp,
+        'x-ncp-iam-access-key': 'm2ly34gJo3yRuuy2TS5y',
+        'x-ncp-apigw-signature-v2': make_signature()
+    }
+    data = {
+        'type': 'SMS',
+        'from': "01032495915",
+        'content': "통신보안 들이나 다진?",
+        'messages': [{'to': "01047314189"}]
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    response.raise_for_status()
+    result = response.json()
+    return result
+
+
+
 
 def reservation(request):
+    # send_sms()
     if request.method == 'POST': #로그인, person, 예약 보장됨
         time_box_id = request.POST.get('cancel_time_box_id')
         time_box = TimeBox.objects.get(id=time_box_id)
 
         current_user = request.user
         person = current_user.person_set.get()
-        
+
         if(person.gender == 1):
             time_box.man = None
             time_box.save()
@@ -41,6 +86,7 @@ def reservation(request):
         time_boxs = 0
         person = 0
     context = {'time_boxs': time_boxs, 'person': person,}
+
     return render(request, "reservation.html", context)
 
 def timebox_detail(request):
@@ -75,6 +121,13 @@ def detail(request, id, gender):
         elif(gender == 2):
             time_box.woman = person
             time_box.save()
+        
+        # if(time_box.man and time_box.woman):
+        #     matched_man = time_box.man_timebox_set.get()
+        #     matched_woman = time_box.woman_timebox_set.get()
+        #     man_phone = matched_man.phone_number
+        #     woman_phone = matched_woman.phone_number
+        #     send_sms(man_phone, woman_phone)
         return redirect(f"/detail/{id}/{gender}")
     else:
         try:
