f2d861fbc028e2b5345d364a653d96c8619fb740
heondo <rundo999@gmail.com>
2023-05-05 14:27:33 +0900

    static- settings.py 수정

--- config/settings.py
+++ config/settings.py
@@ -152,10 +152,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join("staticfiles")
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static/",
+    os.path.join(BASE_DIR, 'static')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
