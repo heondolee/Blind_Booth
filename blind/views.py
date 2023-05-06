b150b7da12eedd69acad1ca050a6dd97bf09c2e1
heondo <rundo999@gmail.com>
2023-05-06 18:09:44 +0900

    ㄹㅇㄴㅁㅇㄹ

--- blind/views.py
+++ blind/views.py
@@ -124,7 +124,7 @@ def detail(request, id, gender):
         elif(gender == 2):
             time_box.woman = person
             time_box.save()
-        
+        messages.warning(request, "매칭 신청이 완료되었습니다.")
         if(time_box.man and time_box.woman):
             if(time_box.day == 1):
                 matched_day = '5/9(화)'
@@ -142,7 +142,7 @@ def detail(request, id, gender):
 
             mached_message = matched_day + matched_slot + ":" + mached_min
             post_message1 = "* 암흑 속 미팅 부스 *\n" + mached_message + "\n매칭성공" 
-            post_message2 = "10000원 입금 후 입금자명을 알려주셔야 예약이 확정됩니다.\n97903132301011 기업은행\n이*도"
+            post_message2 = "10000원 입금 후 입금자명을 문자로 알려주셔야 예약이 확정됩니다.\n97903132301011 기업은행\n이*도"
             my_message = mached_message + "\n남: " + matched_man.phone_number + matched_man.name + "\n여: " + matched_woman.phone_number + matched_woman.name
 
             send_sms('01032495915', my_message)
@@ -168,7 +168,7 @@ def detail(request, id, gender):
             messages.warning(request, "성별이 다릅니다.")
             return redirect(f"/reserve_page/{time_box.day}/{time_box.timeSlot}")
         else:
-            messages.warning(request, "매칭 신청이 완료되었습니다.")
+            pass
     context = {'time_box': time_box, 'gender' : gender}
     return render(request, "detail.html", context=context)
 
